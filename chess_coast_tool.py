6b0ff02d95b5c005bfdf8572e660fee6ed56a418
playerO1 <alekseyk555@mail.ru>
2022-11-27 23:46:45 +0300

    New tool for parse and collect data from games for table processor

---
+++ chess_coast_tool.py
@@ -0,0 +1,289 @@
+#
+# Chess find interesting games. Database tool for : import andexport local database, for fast find games.
+# (C) A.K. 2022 
+# 22.05.2022, 27.11.2022
+#
+
+
+import tools
+from tools import WHITE, BLACK
+import sunfish
+
+
+
+
+# ---------- my logic ------------
+
+# find position with max value of funct()
+
+def scanMaxLength(hist): # wery long game
+    i=0
+    for p,m in hist:
+        i=i+1
+    return i,i
+
+def match_disbalance(pos):
+    price={'p':1,'r':4,'n':3,'b':3,'q':5,'k':0,   'P':-1,'R':-4,'N':-3,'B':-3,'Q':-5,'K':0, }
+    # tools.get_color(pos)
+    # sunfish.piece
+    v=0
+    for i, p in enumerate(pos.board):
+        #if not p.isupper(): continue
+        if p in price:
+            v = v + price[p]
+    #print("debug v=",v," color=",tools.get_color(pos)," pos=", pos)
+    return v
+
+def aggregate_count(pos, swapColor):
+    count={'p':0,'r':0,'n':0,'b':0,'q':0,'k':0,   'P':0,'R':0,'N':0,'B':0,'Q':0,'K':0 }
+    for i, p in enumerate(pos.board):
+        #if not p.isupper(): continue
+        if not p in count: continue # OR count[p]=0
+        if swapColor:
+            p = p.swapcase() #if p.isupper(): p = p.casefold() else: p = p.capitalize()
+        count[p]=count[p]+1
+    #print("debug v=",v," color=",tools.get_color(pos)," pos=", pos)
+    return count
+    
+def scanFinishPosition(hist, funct): # the last game position
+    lastI=0
+    lastP=None
+    for p,m in hist:
+        lastI=lastI+1
+        lastP = p
+    #return lastP,lastI
+    if lastP==None: return 0, 0
+    v = funct(lastP, lastI%2==1)
+    return v, lastI
+
+def plasMapByKey(a,b):
+    #import collections, functools, operator
+    #s = dict(functools.reduce(operator.add,map(collections.Counter, [a, b])))
+    s = {}
+    for k in (a.keys() | b.keys()):
+        s[k] = a.get(k, 0) + b.get(k, 0)
+    return s
+
+def scanCoastSumm(hist): # aggregate_count summ of all
+    lastI=0
+    lastMS=None
+    for p,m in hist:
+        ms=aggregate_count(p, lastI%2==1)
+        if lastI==0: #lastMS==None:
+            lastMS = ms
+        else:
+            lastMS = plasMapByKey(lastMS, ms)
+        lastP = p
+        lastI=lastI+1
+    return lastMS, lastI
+
+def scanMoveCount(hist):
+    count={'p':0,'r':0,'n':0,'b':0,'q':0,'k':0,   'P':0,'R':0,'N':0,'B':0,'Q':0,'K':0 }
+    lastI=0
+    for p,m in hist:
+        f=p.board[m[0]]
+        #print("test",p,m,f)
+        if not f in count: raise Error("Wring figure "+f+" at "+m) #continue # OR count[p]=0
+        swapColor = lastI%2==1
+        if swapColor:
+            f = f.swapcase()
+        count[f]=count[f]+1
+        lastI=lastI+1
+    return count, lastI
+
+# --------------------------
+
+
+#-------- improvement sunfish tool ---------
+
+# maked by rewrite tools.py (sunfish)
+#import re
+def _parse_single_pgn(lines):
+    parts = lines.split() #re.sub('{.*?}', '', ' '.join(lines)).split()
+    msans = [part for part in parts if not part[0].isdigit()]
+    pos0 = tools.parseFEN(tools.FEN_INITIAL)
+    for msan in msans:
+        #print("msan is \"", msan,"\"")
+        try:
+            move = tools.parseSAN(pos0, msan)
+            #print("move is ", move)
+        except AssertionError:
+            print('PGN was:', ' '.join(lines))
+            raise
+        yield pos0, move
+        pos0 = pos0.move(move)
+
+
+# struct: gdata (line:int,name_W,name_B,elo_W:int,elo_B:int,result:int(-1/0/+1),pgn:String, value_?...)
+class GData:
+    line:int=None
+    name_W=None
+    name_B=None
+    elo_W:int=0
+    elo_B:int=0
+    result=None
+    pgn=None # 0,-1 black, +1 white
+    length:int=0
+    data=None # map{}
+    def toMap(self):
+        m={'line':self.line,'name_W':self.name_W,'name_B':self.name_B,'elo_W':self.elo_W,'elo_B':self.elo_B,'result':self.result,'length':self.length,'pgn':self.pgn}
+        if self.data!=None:
+            m = dict(m.items() | self.data.items()) #  + same as |  . TODO add 'var_' prefix
+        return m
+
+
+def game_filter(gdata:GData):
+    if (abs( gdata.elo_W-gdata.elo_B)>200): return False
+    if ((gdata.elo_W+gdata.elo_B)/2<600): return False
+    if (len(gdata.pgn)<10): return False
+    #if (time_control > 30)
+    if (gdata.result==0): return False
+    return True
+
+import re #reg expression
+def parse_game_data(meta_str, lineN:int, pgn_str):
+    gdata=GData()
+    gdata.line=lineN
+    gdata.pgn=pgn_str.replace('\n', ' ') #TODO space or empty for new line? It may be trouble when parsed next
+    #s='[White "hadwao"]'
+    if match := re.search('\[White "(.*)"\]', meta_str, re.IGNORECASE):
+        gdata.name_W = match.group(1)
+    if match := re.search('\[Black "(.*)"\]', meta_str, re.IGNORECASE):
+        gdata.name_B = match.group(1)
+    if match := re.search('\[WhiteElo "(.*)"\]', meta_str, re.IGNORECASE):
+        eloS = match.group(1)
+        if '?' == eloS or ''== eloS:
+            gdata.elo_W = -1
+        else:
+            gdata.elo_W = int(eloS)
+    if match := re.search('\[BlackElo "(.*)"\]', meta_str, re.IGNORECASE):
+        eloS = match.group(1)
+        if '?' == eloS or ''== eloS:
+            gdata.elo_B = -1
+        else:
+            gdata.elo_B = int(eloS)
+    #[Termination "Normal"] [Termination "Time forfeit"]
+    #[Result "1-0"] - in comment or PGN.
+    if match := re.search('\[Result "(.*)"\]', meta_str, re.IGNORECASE):
+        gres=match.group(1)
+        if (gres=="1-0"): gdata.result = 1#WHITE
+        elif (gres=="0-1"): gdata.result = -1#BLACK
+        elif (gres=="1/2-1/2"): gdata.result = 0
+        else:
+            print("Warn undefined result:", gres)
+            gdata.result = gres
+    #print("META LIKE:", meta_str)
+    #print("line ", gdata.line," white:" ,gdata.name_W," ",gdata.elo_W," black:" ,gdata.name_B," ",gdata.elo_B," result=",gdata.result )
+    return gdata
+
+# funct: val, moveI = scanAll(hist, funct)
+# return yield gdata
+def process_file_PGN(file_name, funct):
+    lI=0
+    current_game_meta = ""
+    prev_meta=False
+    #print("line\tmove\tvalue")
+    maxL=None
+    total_games_success=0
+    total_games_errors=0
+    
+    with open(file_name, newline='') as lines:
+        for line in lines:
+            lI=lI+1
+            #print(lI)#,'="',line,'"', len(line))
+            if line.startswith('['):
+                current_game_meta = current_game_meta + line
+                prev_meta=True
+            elif len(line)<3 or not line[0].isdigit():
+                pass
+            else:
+                #if len(line)<6: continue # сразу сдались
+                #if not('1-0' in line or '0-1' in line): continue # без ничьих
+                gdata = parse_game_data(current_game_meta, lI, line);
+                if not game_filter(gdata): #TODO add filter function form param
+                    if prev_meta:
+                        current_game_meta=""
+                        prev_meta=False
+                    continue
+                try:
+                    hist=_parse_single_pgn(line)
+                    val, moveI = funct(hist) #scanAll(hist, funct)
+                    #gdata.data=funct(hist) #scanAll(hist, funct)
+                    gdata.data=val
+                    gdata.length=moveI
+                    print("success line {0}".format(lI))
+                    yield gdata
+
+#                    if prev_meta:
+#                        current_game_meta=""
+#                        prev_meta=False
+                    total_games_success=total_games_success+1
+                    maxL=line
+                except AssertionError as err:
+                    print("Error parse line {0} cause {1}".format(lI, err))
+                    total_games_errors=total_games_errors+1
+                if prev_meta:
+                    current_game_meta=""
+                    prev_meta=False
+    print("--- End of file, processed {0} games + error parsing {1} games ---".format(total_games_success, total_games_errors))
+
+
+# parser, filter, file-maker
+
+
+#-----------------
+
+import csv
+def main():
+    import sys
+    import os
+    if len(sys.argv)!=3:
+        print("Required PGN file. Use python myposfinder.py path/to_file.pgn")
+        return
+    file_name_pgn=sys.argv[1]
+    if not os.path.isfile(file_name_pgn):
+        print("Required PGN file. File not found: ", file_name_pgn)
+        return
+    file_name_out=sys.argv[2]
+    print("--- PGN file:",file_name_pgn,"  out:",file_name_out," ---")
+
+    #The sort of collect data of the game:    
+    #functGame=lambda hist: scanFinishPosition(hist, aggregate_count) # get last position, count of figure
+    #functGame=scanCoastSumm # scan all positions, average figure by timeline
+    functGame=scanMoveCount
+
+    resultIter = process_file_PGN(file_name_pgn, functGame)
+    
+    firstRow=True
+    with open(file_name_out, 'w', newline='') as csvfile:
+        #out_writer = csv.writer(csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        c_writer = None # csv.DictWriter(csvfile, fieldnames=fieldnames)
+        for gdata in resultIter:
+            #print("line ", gdata.line," white:" ,gdata.name_W," ",gdata.elo_W," black:" ,gdata.name_B," ",gdata.elo_B, "result=",gdata.result, " PGN=",gdata.pgn, " data=",gdata.data )
+            #print("line: ", gdata.toMap() )
+            row=gdata.toMap()
+            if firstRow:
+                # header
+                c_writer = csv.DictWriter(csvfile, fieldnames=row.keys())
+                c_writer.writeheader()
+                firstRow = False
+            c_writer.writerow(row)
+            #spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
+
+
+        
+    print("--- finish ---")
+
+if __name__ == '__main__':
+    main()
+
+
+#TODO: perfomance optimization
+
+#Excel/OpenOffice: https://wiki.openoffice.org/wiki/Documentation/How_Tos/Calc:_LINEST_function
+#LINEST(yvalues; xvalues; allow_const; stats) =ЛИНЕЙН(C$2:C$5;A$2:B$5;1;1)  Ctrl+Shift+Enter
+#ЛИНЕИН()...
+#Legend: P-пешка, R-ладья, N-конь, B - офицер, Q - ферзь, K - король , большие буквы - белые, 
+#in sunfish.py: piece = { 'P': 100, 'N': 280, 'B': 320, 'R': 479, 'Q': 929, 'K': 60000 }
+# PPPPPPPP\n'  #  80 - 89
+# RNBQKBNR
---
+++ chess_coast_tool_test.py
@@ -0,0 +1,151 @@
+#
+# Chess find interesting games.
+# (C) A.K. 2022 
+# 27.11.2022
+#
+
+# test for chess_coast_tool.py
+
+
+import chess_coast_tool
+import tools
+#from tools import WHITE, BLACK
+#import sunfish
+
+
+
+
+#-----------------
+
+
+#if __name__ == '__main__':
+#    main()
+
+def check_equals(exp,val, test_name=''):
+    if (exp!=val): raise AssertionError('Expected \"'+str(exp)+'\" <> value \"'+str(val)+'\" test error found! '+test_name)
+
+def moves_to_str(parsed_pgn):
+    s=""
+    n=0
+    for pos,move in parsed_pgn:
+        s=s+tools.mrender(pos,move)+' ' # str(move) or tools.mrender(pos,move)
+        n=n+1
+    return s+'('+str(n)+' move)'
+
+print("--- Unit test for chess_coast_tool.py ---")
+
+# === parsing test (see also chessposfinder.py) ===
+
+
+print("Test _parse_single_pgn() 1")
+hist=chess_coast_tool._parse_single_pgn('')
+check_equals('(0 move)', moves_to_str(hist), 'PGN parse test 1')
+
+print("Test _parse_single_pgn() 2 www.lichess.com format")
+hist=chess_coast_tool._parse_single_pgn("1. e4 b6 2. d4 d6 3. Nc3 Nc6 4. d5 Qd7 5. dxc6 Qxc6 6. Bb5 Qxb5 7. Nxb5 Bb7 8. Nxc7+ Kd8 9. Nxa8 e5 1-0")
+check_equals('e2e4 b7b6 d2d4 d7d6 b1c3 b8c6 d4d5 d8d7 d5c6 d7c6 f1b5 c6b5 c3b5 c8b7 b5c7 e8d8 c7a8 e7e5 (18 move)', moves_to_str(hist), 'PGN parse test 2')
+
+#print("Test _parse_single_pgn() 3 www.pgnmentor.com format")
+#hist=chess_coast_tool._parse_single_pgn("""1.c4 g6 2.d4 Bg7 3.e4 d6 4.Nc3 c5 5.dxc5 Bxc3+ 6.bxc3 dxc5 7.Bd3 Nc6 8.f4 Qa5
+#9.Ne2 Be6 10.f5 O-O-O 11.fxe6 Ne5 12.exf7 Nf6 13.O-O Nxd3 14.Bh6 Ne5 15.Qb3 Nxf7  1-0""")
+#check_equals('c2c4 g7g6 d2d4 f8g7 e2e4 d7d6 b1c3 c7c5 d4c5 g7c3 b2c3 d6c5 f1d3 b8c6 f2f4 d8a5 g1e2 c8e6 f4f5 e8c8 f5e6 c6e5 e6f7 g8f6 e1g1 e5d3 c1h6 #d3e5 d1b3 e5f7 (30 move)', moves_to_str(hist), 'PGN parse test 3')
+
+print("Test _parse_single_pgn() 4.1 мат в 5 ходов начало чёрными")
+hist=chess_coast_tool._parse_single_pgn("e4 f6 Qe2 g5 Qh5# 1-0")
+check_equals('e2e4 f7f6 d1e2 g7g5 e2h5 (5 move)', moves_to_str(hist), 'PGN parse test 4.1')
+
+print("Test _parse_single_pgn() 4.2 мат в 4 хода начало белыми")
+hist=chess_coast_tool._parse_single_pgn("f3 e6 g4 Qh4# 0-1")
+check_equals('f2f3 e7e6 g2g4 d8h4 (4 move)', moves_to_str(hist), 'PGN parse test 4.2')
+
+#print("Test _parse_single_pgn() 4.3 чёрные отдаютвсе фигуры и проигрывают сдаются")
+#hist=chess_coast_tool._parse_single_pgn("1.d4 Nf6 2.Nf3 Ne4 3.e3 e6 4.Nbd2 Bb4 5.c3 Nc6 6.Nxe4 Qh4 7.Nxh4 b6 8.cxb4 Ba6 9.Bxa6 Ne5 10.dxe5 Rc8 11.Bxc8 h5 12.Qxd7+ 1-0")
+#check_equals('d2d4 g8f6 g1f3 f6e4 e2e3 e7e6 b1d2 f8b4 c2c3 b8c6 d2e4 d8h4 f3h4 b7b6 c3b4 c8a6 f1a6 c6e5 d4e5 a8c8 a6c8 h7h5 d1d7 (23 move)', moves_to_str(hist), 'PGN parse test 4.3')
+
+print("Test _parse_single_pgn() 4.4 игра и ничья по согласию")
+hist=chess_coast_tool._parse_single_pgn("1. b3 d5 2. a4 Nf6 3. Ba3 e5 4. f3 Bxa3 5. Nxa3 Nc6 6. g4 Be6 7. Nh3 Qd6 8. Bg2 O-O-O 9. d3 d4 10. O-O h6 11. Qd2 g5 12. Nb5 Qb4 13. Qxb4 Nxb4 14. c4 Nc2 15. Rac1 Ne3 16. Rfe1 Rd6 17. c5 1/2-1/2")
+check_equals('b2b3 d7d5 a2a4 g8f6 c1a3 e7e5 f2f3 f8a3 b1a3 b8c6 g2g4 c8e6 g1h3 d8d6 f1g2 e8c8 d2d3 d5d4 e1g1 h7h6 d1d2 g7g5 a3b5 d6b4 d2b4 c6b4 c2c4 b4c2 a1c1 c2e3 f1e1 d8d6 c4c5 (33 move)', moves_to_str(hist), 'PGN parse test 4.4')
+
+print("Test parse_game_data() 5.1")
+test_str_meta="""[Event "Monthly Blitz Arena"]
+[Site "https://lichess.org/VDbaLr56"]
+[Date "2022.04.27"]
+[White "ajaykthakur22pm"]
+[Black "Qutrippen"]
+[Result "1-0"]
+[UTCDate "2022.04.27"]
+[UTCTime "20:58:43"]
+[WhiteElo "1576"]
+[BlackElo "1216"]
+[WhiteRatingDiff "+2"]
+[BlackRatingDiff "-2"]
+[Variant "Standard"]
+[TimeControl "300+0"]
+[ECO "B00"]
+[Termination "Normal"]
+"""
+test_str_pgn="1. e4 e5 2. d4 Nc6 3. d5 Nd4 4. Nf3 Nxf3+ 5. Qxf3 d6 6. Nc3 Nf6 7. Bb5+ Bd7 8. Bd3 g6 9. h3 Bg7 10. Bg5 Qe7 11. Nb5 Bxb5 12. Bxb5+ Kf8 13. h4 a6 14. Bd3 Nh5 15. Bxe7+ Kxe7 1-0"
+gdata=chess_coast_tool.parse_game_data(test_str_meta, 100, test_str_pgn)  # type GData
+check_equals(100, gdata.line, 'PGN parse result test 5.1')
+check_equals(test_str_pgn, gdata.pgn, 'PGN parse result test 5.1')
+check_equals("ajaykthakur22pm", gdata.name_W, 'PGN parse result test 5.1')
+check_equals("Qutrippen", gdata.name_B, 'PGN parse result test 5.1')
+check_equals(1576, gdata.elo_W, 'PGN parse result test 5.1')
+check_equals(1216, gdata.elo_B, 'PGN parse result test 5.1')
+check_equals(1, gdata.result, 'PGN parse result test 5.1') # 1=WHITE
+
+print("Test game_filter() 5.2")
+check_equals(False, chess_coast_tool.game_filter(gdata), 'PGN parse result test 5.2') # not allow this game - elo difference too much
+#TODO more test for game_filter
+
+
+
+
+# === math result test ===
+
+# TODO unittest:!
+
+print("Test scanFinishPosition aggregate_count() 20.1")
+pgn="e4 f6 Qe2 g5 Qh5# 1-0"
+hist=chess_coast_tool._parse_single_pgn(pgn)
+functGame=lambda hist: chess_coast_tool.scanFinishPosition(hist, chess_coast_tool.aggregate_count)
+check_equals(({'p': 8, 'r': 2, 'n': 2, 'b': 2, 'q': 1, 'k': 1, 'P': 8, 'R': 2, 'N': 2, 'B': 2, 'Q': 1, 'K': 1}, 5), functGame(hist), 'Math of game, 20.1')
+
+#TODO test functGame with difference values
+
+
+print("Test scanCoastSumm() 20.3")
+pgn="f3 e6 g4 Qh4# 0-1"
+hist = chess_coast_tool._parse_single_pgn(pgn)
+check_equals(({'p': 32, 'N': 8, 'P': 32, 'k': 4, 'n': 8, 'b': 8, 'r': 8, 'q': 4, 'R': 8, 'K': 4, 'Q': 4, 'B': 8}, 4), chess_coast_tool.scanCoastSumm(hist), 'Math of game, 20.3')
+#TODO test scanCoastSumm with difference values
+
+
+print("Test scanMoveCount() 20.5")
+pgn="f3 e6 g4 Qh4# 0-1"
+hist = chess_coast_tool._parse_single_pgn(pgn)
+check_equals(({'p': 1, 'r': 0, 'n': 0, 'b': 0, 'q': 1, 'k': 0, 'P': 2, 'R': 0, 'N': 0, 'B': 0, 'Q': 0, 'K': 0}, 4), chess_coast_tool.scanMoveCount(hist), 'Math of game, 20.5')
+#TODO test scanMoveCount with difference values
+
+
+
+
+
+print("--- All unit-test done success ---")
+
+
+
+print("--- Perfomance test ---")
+# отладка, профайл и дамп памяти:
+#TODO rewrite perfomance.
+import cProfile
+def test_perfomance_scanAllPosition_1():
+    pgn="1.d4 Nf6 2.Nf3 Ne4 3.e3 e6 4.Nbd2 Bb4 5.c3 Nc6 6.Nxe4 Qh4 7.Nxh4 b6 8.cxb4 Ba6 9.Bxa6 Ne5 10.dxe5 Rc8 11.Bxc8 h5 1-0"
+    hist = chess_coast_tool._parse_single_pgn(pgn)
+    result_value, result_i = chess_coast_tool.scanMoveCount(hist, chessposfinder.match_koef_razmen_any)
+    check_equals((3,10), (result_value, result_i), 'Validate perfomance test')
+print("Test perfomance scanAllPosition() and match_koef_razmen_any()")
+cProfile.run('test_perfomance_scanAllPosition_1()')
+#from guppy import hpy; h=hpy()  #pip3 install guppy3
+#h.heap()
+
